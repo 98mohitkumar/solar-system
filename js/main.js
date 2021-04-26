acc1da4c12f9d9e9f207c3327e36f7ec7c183e7c
98mohitkumar <98mohitkumar@gmail.com>
2021-04-26 12:48:23 +0530

    added phobos and deimos

--- js/main.js
+++ js/main.js
@@ -24,6 +24,66 @@ Webflow.push(function () {
   });
 });
 
-mediumZoom("[data-zoomable]", {
-  margin: 24,
-});
+//  Phobos
+var modalbtnPhobos = document.getElementsByClassName("modalbtnPhobos")[0];
+
+var modalPhobos = document.getElementById("phobos_overlay");
+
+var closebtnPhobos = document.getElementsByClassName("closebtnPhobos")[0];
+
+modalbtnPhobos.addEventListener("click", openmodalPhobos);
+
+closebtnPhobos.addEventListener("click", closemodalPhobos);
+
+window.addEventListener("click", outsideclickPhobos);
+
+function openmodalPhobos() {
+  phobos_overlay.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+
+function closemodalPhobos() {
+  {
+    phobos_overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+function outsideclickPhobos(e) {
+  if (e.target == phobos_overlay) {
+    phobos_overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+//  Deimos
+var modalbtnDeimos = document.getElementsByClassName("modalbtnDeimos")[0];
+
+var modalDeimos = document.getElementById("deimos_overlay");
+
+var closebtnDeimos = document.getElementsByClassName("closebtnDeimos")[0];
+
+modalbtnDeimos.addEventListener("click", openmodalDeimos);
+
+closebtnDeimos.addEventListener("click", closemodalDeimos);
+
+window.addEventListener("click", outsideclickDeimos);
+
+function openmodalDeimos() {
+  deimos_overlay.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+
+function closemodalDeimos() {
+  {
+    deimos_overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
+
+function outsideclickDeimos(e) {
+  if (e.target == deimos_overlay) {
+    deimos_overlay.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+}
