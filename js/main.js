2fe5341efe2fa078776c88ccdd961b2b237f9700
98mohitkumar <98mohitkumar@gmail.com>
2022-12-02 10:51:45 +0530

    iframe fixes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Smooth Scrolling with offset
-$(".sections a, .btn a, .logo a").on("click", function (event) {
-  if (this.hash !== "") {
+$('.sections a, .btn a, .logo a').on('click', function (event) {
+  if (this.hash !== '') {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $("html, body").animate(
+    $('html, body').animate(
       {
-        scrollTop: $(hash).offset().top - 80,
+        scrollTop: $(hash).offset().top - 80
       },
       800
     );
@@ -17,109 +17,109 @@ $(".sections a, .btn a, .logo a").on("click", function (event) {
 // Remove anchor from URl bar
 var Webflow = Webflow || [];
 Webflow.push(function () {
-  $("a").click(function () {
+  $('a').click(function () {
     setTimeout(function () {
-      history.replaceState(null, null, " ");
+      history.replaceState(null, null, ' ');
     }, 0);
   });
 });
 
 // Hamburger Menu
 
-const hamButton = document.querySelector(".hamwrap");
+const hamButton = document.querySelector('.hamwrap');
 
-const navbarLinks = document.querySelector(".navbar-links ul");
+const navbarLinks = document.querySelector('.navbar-links ul');
 
-const closeHam = document.querySelector(".navbar-links");
+const closeHam = document.querySelector('.navbar-links');
 
-const cross = document.querySelector(".first");
+const cross = document.querySelector('.first');
 
-const cross1 = document.querySelector(".second");
+const cross1 = document.querySelector('.second');
 
-const cross2 = document.querySelector(".third");
+const cross2 = document.querySelector('.third');
 
-hamButton.addEventListener("click", (e) => {
+hamButton.addEventListener('click', (e) => {
   e.preventDefault();
-  navbarLinks.classList.toggle("navbar-links_active");
-  cross.classList.toggle("f");
-  cross1.classList.toggle("s");
-  cross2.classList.toggle("t");
-  closeHam.classList.toggle("navbar-links_expanded");
+  navbarLinks.classList.toggle('navbar-links_active');
+  cross.classList.toggle('f');
+  cross1.classList.toggle('s');
+  cross2.classList.toggle('t');
+  closeHam.classList.toggle('navbar-links_expanded');
 });
 
-closeHam.addEventListener("click", () => {
-  document.querySelector(".hamwrap").click();
+closeHam.addEventListener('click', () => {
+  document.querySelector('.hamwrap').click();
 });
 
-window.addEventListener("scroll", () => {
-  navbarLinks.classList.remove("navbar-links_active");
-  hamButton.classList.remove("hamwrap_active");
-  cross.classList.remove("f");
-  cross1.classList.remove("s");
-  cross2.classList.remove("t");
-  closeHam.classList.remove("navbar-links_expanded");
+window.addEventListener('scroll', () => {
+  navbarLinks.classList.remove('navbar-links_active');
+  hamButton.classList.remove('hamwrap_active');
+  cross.classList.remove('f');
+  cross1.classList.remove('s');
+  cross2.classList.remove('t');
+  closeHam.classList.remove('navbar-links_expanded');
 });
 
 //  Phobos
-var modalbtnPhobos = document.getElementsByClassName("modalbtnPhobos")[0];
+const modalbtnPhobos = document.getElementsByClassName('modalbtnPhobos')[0];
 
-var modalPhobos = document.getElementById("phobos_overlay");
+const modalPhobos = document.getElementById('phobos_overlay');
 
-var closebtnPhobos = document.getElementsByClassName("closebtnPhobos")[0];
+const closebtnPhobos = document.getElementsByClassName('closebtnPhobos')[0];
 
-modalbtnPhobos.addEventListener("click", openmodalPhobos);
+modalbtnPhobos.addEventListener('click', openmodalPhobos);
 
-closebtnPhobos.addEventListener("click", closemodalPhobos);
+closebtnPhobos.addEventListener('click', closemodalPhobos);
 
-window.addEventListener("click", outsideclickPhobos);
+window.addEventListener('click', outsideclickPhobos);
 
 function openmodalPhobos() {
-  phobos_overlay.style.display = "block";
-  document.body.style.overflow = "hidden";
+  phobos_overlay.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 }
 
 function closemodalPhobos() {
   {
-    phobos_overlay.style.display = "none";
-    document.body.style.overflow = "auto";
+    phobos_overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 }
 
 function outsideclickPhobos(e) {
   if (e.target == phobos_overlay) {
-    phobos_overlay.style.display = "none";
-    document.body.style.overflow = "auto";
+    phobos_overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 }
 
 //  Deimos
-var modalbtnDeimos = document.getElementsByClassName("modalbtnDeimos")[0];
+var modalbtnDeimos = document.getElementsByClassName('modalbtnDeimos')[0];
 
-var modalDeimos = document.getElementById("deimos_overlay");
+var modalDeimos = document.getElementById('deimos_overlay');
 
-var closebtnDeimos = document.getElementsByClassName("closebtnDeimos")[0];
+var closebtnDeimos = document.getElementsByClassName('closebtnDeimos')[0];
 
-modalbtnDeimos.addEventListener("click", openmodalDeimos);
+modalbtnDeimos.addEventListener('click', openmodalDeimos);
 
-closebtnDeimos.addEventListener("click", closemodalDeimos);
+closebtnDeimos.addEventListener('click', closemodalDeimos);
 
-window.addEventListener("click", outsideclickDeimos);
+window.addEventListener('click', outsideclickDeimos);
 
 function openmodalDeimos() {
-  deimos_overlay.style.display = "block";
-  document.body.style.overflow = "hidden";
+  deimos_overlay.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 }
 
 function closemodalDeimos() {
   {
-    deimos_overlay.style.display = "none";
-    document.body.style.overflow = "auto";
+    deimos_overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 }
 
 function outsideclickDeimos(e) {
   if (e.target == deimos_overlay) {
-    deimos_overlay.style.display = "none";
-    document.body.style.overflow = "auto";
+    deimos_overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
   }
 }
